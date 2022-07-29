c290d5c690203764af5e1a775dc39b3dbb1a709f
Dominik Salwerowicz <9930850-zciwokini@users.noreply.gitlab.com>
2022-07-29 11:09:42 +0200

    Renamed DebugConsole to IngameConsole Added it as a singleton via the plugin script

--- addons/ingame_console/plugin.gd
+++ addons/ingame_console/plugin.gd
@@ -4,11 +4,13 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type(
-		"DebugConsole", "Control",
+		"IngameConsole", "Control",
 		preload("res://addons/ingame_console/scripts/console_gui.gd"),
 		preload("res://addons/ingame_console/images/console_icon.svg")
 	)
+	add_autoload_singleton("Console", "res://addons/ingame_console/scripts/console_global.gd")
 
 
 func _exit_tree():
-	remove_custom_type("DebugConsole")
+	remove_custom_type("IngameConsole")
+	remove_autoload_singleton("Console")
