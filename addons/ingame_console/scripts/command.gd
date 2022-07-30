a4dda2fb20bfcd825b2d7d2b7bf6529d761ffadf
Dominik Salwerowicz <9930850-zciwokini@users.noreply.gitlab.com>
2022-07-30 10:19:58 +0200

    Cleaned up arguments' parsing

--- addons/ingame_console/scripts/command.gd
+++ addons/ingame_console/scripts/command.gd
@@ -46,37 +46,43 @@ func parse_string_args(text: String) -> Dictionary:
 	
 	text = text.lstrip(id)
 	
-	var state := "Searching Key"
+	var STATES := {
+		SEARCHING_KEY = "Searching Key",
+		BUILDING_KEY = "Building Key",
+		BUILDING_VALUE = "Building Value"
+	}
+	var state: String = STATES.SEARCHING_KEY
 	var current_key := ""
 	var is_string_value := false
 	var current_value := ""
 	
 	var i := 0
 	while i < text.length():
+#	for i in range(text.length()):
 		var c = text[i]
 		match state:
-			"Searching Key":
+			STATES.SEARCHING_KEY:
 				if c != " ":
-					i -= 1
-					state = "Building Key"
-		
-			"Building Key":
-				if text.substr(i, 1) == "=":
-					state = "Building Value"
+					current_key = c
+					state = STATES.BUILDING_KEY
+			
+			STATES.BUILDING_KEY:
+				if c == "=":
+					state = STATES.BUILDING_VALUE
 					if text.substr(i, 2) == "=\"":
 						i += 1
 						is_string_value = true
-				elif text.substr(i, 1) == " ":
+				elif c == " ":
 					args[current_key] = ""
 					current_key = ""
-					state = "Searching Key"
+					state = STATES.SEARCHING_KEY
 				elif i == text.length() - 1:
 					current_key += c
 					args[current_key] = ""
 				else:
 					current_key += c
-		
-			"Building Value":
+			
+			STATES.BUILDING_VALUE:
 				if (is_string_value and c == "\"") or (not is_string_value and c == " ") or (i == text.length() - 1):
 					if i == text.length() - 1 and c != "\"":
 						current_value += c
@@ -84,7 +90,7 @@ func parse_string_args(text: String) -> Dictionary:
 					current_key = ""
 					current_value = ""
 					is_string_value = false
-					state = "Searching Key"
+					state = STATES.SEARCHING_KEY
 				else:
 					current_value += c
 		
